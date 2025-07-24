0ffa53bb941d0836e630ae514ee6a3125e7aa71f
Fin-Headley <Finy1@mac.com>
2025-07-24 22:16:41 +0000

    first commit-test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Analysis of GW190521")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.markdown(
+     """ 
+    
+    This will be my initial page.
+
+    I will use this as a sudo README.
+
+    I will define pages from here that split out and do different things.
+     """ 
+    )
