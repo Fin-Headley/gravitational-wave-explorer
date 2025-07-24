bf0efc79e9f6f70275038a7549b50ce4ba20d8fb
Fin-Headley <Finy1@mac.com>
2025-07-24 22:27:43 +0000

    Creating a Testpage

---
+++ pages/testpage1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("testpage1")
+st.write(
+    "this is a test page."
+)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+
+pg = st.navigation([st.Page("testpage1.py")])
+pg.run()
+
+#st.set_page_config(
+#    page_title="Hello",
+#    page_icon="",
+#)
+
+
 st.title("Analysis of GW190521")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
