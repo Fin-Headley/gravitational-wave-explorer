9e9cd5b84ef7c2d76e2bd11020d102507ada037e
Fin-Headley <Finy1@mac.com>
2025-07-28 18:16:34 -0400

    Update 2-data_test.py to test plotting and remerge branches

--- 2-data_test.py
+++ 2-data_test.py
@@ -24,3 +24,57 @@ st.title("testpage2")
 st.write(
     "this is a test page 2."
 )
+
+#code
+#gets the time of the event and prints it
+gps = event_gps('GW190521')
+st.write("Time of event:",gps)
+st.write("Time of event:",from_gps(gps))
+segment = ((gps)-16, (gps)+16)
+st.write("Period of time with event in it:",segment)
+
+time_center = gps
+
+
+
+data = {}
+
+ifos = ['L1', 'V1', 'H1']
+
+for ifo in ifos:
+    filename = f"/GW190521_data/{ifo}_data_32s.hdf5"
+    data[ifo] = TimeSeries.read(filename)
+
+
+colours = {}
+colours['H1'] = 'gwpy:ligo-hanford'
+colours['L1'] = 'gwpy:ligo-livingston'
+colours['V1'] = 'gwpy:virgo'
+
+labels ={}
+labels['H1'] = 'LIGO-Hanford'
+labels['L1'] = 'LIGO-Livingston'
+labels['V1'] = 'Virgo'
+
+ldata = data['L1']
+hdata = data['H1']
+vdata = data['V1']
+
+st.write("Data type:       ", type(ldata))
+st.write("Data duration:   ",ldata.duration)
+st.write("Data Sample Rate:",ldata.sample_rate)
+st.write("Data delta t:    ",ldata.dt)
+st.write("Data start time: ",ldata.x0)
+
+plot = GWPlot(figsize=(12, 4.8))
+ax = plot.add_subplot(xscale='auto-gps')
+
+for ifo in ifos:
+    ax.plot(data[ifo],label=labels[ifo],color=colours[ifo])
+
+ax.set_epoch(time_center) # type: ignore
+
+ax.set_title('GW Strain data from our three Detectors')
+ax.set_ylabel('Strain noise')
+ax.legend(loc="best")
+plt.show()
