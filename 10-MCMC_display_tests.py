dfaf37d82973ed4b61e89807be6066e9909f32c7
Fin-Headley <Finy1@mac.com>
2025-10-22 03:31:56 +0000

    add nbformat to requirements for plotly express

--- 10-MCMC_display_tests.py
+++ 10-MCMC_display_tests.py
@@ -61,21 +61,68 @@ reduced_data = az.from_netcdf("MCMC_processed_data.nc")
 
 stacked = reduced_data.stack(sample=["chain", "draw"], inplace=False)
 
-#Mass = stacked.posterior.Mass.values
-#Ratio = stacked.posterior.Ratio.values
-#Distance = stacked.posterior.Distance.values
-#TimeShift = stacked.posterior.TimeShift.values
-#Phase = stacked.posterior.Phase.values
-#RA = stacked.posterior.RA.values
-#Dec = stacked.posterior.Dec.values
-#Incl = stacked.posterior.Incl.values
-#Pol = stacked.posterior.Pol.values
+Mass = stacked.posterior.Mass.values
+Ratio = stacked.posterior.Ratio.values
+Distance = stacked.posterior.Distance.values
+TimeShift = stacked.posterior.TimeShift.values
+Phase = stacked.posterior.Phase.values
+RA = stacked.posterior.RA.values
+Dec = stacked.posterior.Dec.values
+Incl = stacked.posterior.Incl.values
+Pol = stacked.posterior.Pol.values
 
 #test_array = {}
 #test_array["Mass"] = Mass
 #test_array["Ratio"] = Ratio
 #est_array["Distance"] = Distance
 #test_array
+
+#fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.05,shared_yaxes=True)
+#fig_resampler = FigureResampler(fig)
+
+
+mcmc_df = pd.read_parquet("reduced_mcmc_results.parquet")
+
+def plot_hist(fig,x_data,plot_title,x_axis_title,nbins=50):
+    fig.add_trace(go.Histogram(x=x_data,nbinsx=nbins,histnorm='probability density'),limit_to_view=True,max_n_samples = 60000)
+
+    fig.update_layout(
+        # Hover settings
+        hovermode='x unified',
+        #hoversubplots="axis",
+        width=500,
+        height=500,
+        title={
+                    'text': plot_title,
+                    'y': 0.9,
+                    'x': .5,
+                    'xanchor': 'center',
+                    'yanchor': 'top',
+                    'automargin': True
+                },
+
+        yaxis=dict(
+                title="Probability Density"),
+
+        xaxis=dict(
+            title=x_axis_title)
+
+    )
+
+test_hist = create_new_figure()
+plot_hist(test_hist,mcmc_df["Mass"],"Mass","Mass (Solar Mass)")
+
+st.plotly_chart(test_hist, theme="streamlit",on_select="rerun",use_container_width=True)
+
+
+import plotly.express as px
+df = px.data.tips()
+
+Mass_Ratio = mcmc_df[["Mass", "Ratio"]]
+
+fig = px.density_heatmap(Mass_Ratio, x="Mass", y="Ratio", marginal_x="histogram", marginal_y="histogram")
+fig.show()
+
 
 #corner_test = corner.corner(test_array)
 
